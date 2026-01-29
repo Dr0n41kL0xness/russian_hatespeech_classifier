39e1ca73c5593b0e6f66dde0d61a24c7510b36f8
Dr0n41kL0xness <126822718+Dr0n41kL0xness@users.noreply.github.com>
2026-01-29 23:27:36 +0300

    Update predict.py (added path to HF)

--- predict.py
+++ predict.py
@@ -1,41 +1,25 @@
 import torch
-from transformers import AutoTokenizer
-from src.model import BertClassifier
+from transformers import BertTokenizer, BertForSequenceClassification
 
-def get_prediction(text, model, tokenizer, device):
-    encoding = tokenizer.encode_plus(
-        text,
-        add_special_tokens=True,
-        max_length=512,
-        return_token_type_ids=False,
-        padding='max_length',
-        truncation=True,
-        return_attention_mask=True,
-        return_tensors='pt',
-    )
+model_name = "Dr0n41k/rubert-toxic-classifier"
 
-    input_ids = encoding['input_ids'].to(device)
-    attention_mask = encoding['attention_mask'].to(device)
-
-    with torch.no_grad():
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        _, prediction = torch.max(outputs, dim=1)
-
-    return "Toxic" if prediction.item() == 1 else "Neutral"
-
-if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_name = "DeepPavlov/rubert-base-cased"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = BertClassifier(model_name)
-    
-    
-    model.to(device)
+def predict(text):
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertForSequenceClassification.from_pretrained(model_name)
     model.eval()
 
-    print("Проверка классификатора Hate Speech")
-    #тестовый текст
-    user_input = "Курс доллара стабильно растет,что же нам делать?"
-    result = get_prediction(user_input, model, tokenizer, device)
-    print(f"Текст: {user_input}")
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=128)
+    
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+    prediction = torch.argmax(probs, dim=-1).item()
+    
+    return "Toxic" if prediction == 1 else "Neutral"
+
+if __name__ == "__main__":
+    test_text = "Курс доллара стабильно растет, что же нам делать?"
+    result = predict(test_text)
+    print(f"Текст: {test_text}")
     print(f"Результат: {result}")
